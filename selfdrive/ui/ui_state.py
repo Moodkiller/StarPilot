b0bb10bc55430470ea872bfb8480e0346a207aaa
Moodkiller <appleipodsrock@gmail.com>
2026-09-13 11:54:46 +1200

    Fix for Screen Brightness not working

--- selfdrive/ui/ui_state.py
+++ selfdrive/ui/ui_state.py
@@ -349,29 +349,31 @@ class Device:
       brightness = BACKLIGHT_OFFROAD
     self._offroad_brightness = min(max(brightness, 0), 100)
 
-  def _update_brightness(self):
-    clipped_brightness = self._offroad_brightness
+ def _update_brightness(self):
+    key = "screen_brightness_onroad" if ui_state.started else "screen_brightness"
+    manual = ui_state.starpilot_toggles.get(key, 101)
 
-    if ui_state.started and ui_state.light_sensor >= 0:
-      clipped_brightness = ui_state.light_sensor
-
-      # CIE 1931 - https://www.photonstophotos.net/GeneralTopics/Exposure/Psychometric_Lightness_and_Gamma.htm
-      if clipped_brightness <= 8:
-        clipped_brightness = clipped_brightness / 903.3
-      else:
-        clipped_brightness = ((clipped_brightness + 16.0) / 116.0) ** 3.0
-
-      clipped_brightness = float(np.interp(clipped_brightness, [0, 1], [30, 100]))
+    if manual != 101:
+        clipped_brightness = manual
+    elif ui_state.started and ui_state.light_sensor >= 0:
+        clipped_brightness = ui_state.light_sensor
+        if clipped_brightness <= 8:
+            clipped_brightness = clipped_brightness / 903.3
+        else:
+            clipped_brightness = ((clipped_brightness + 16.0) / 116.0) ** 3.0
+        clipped_brightness = float(np.interp(clipped_brightness, [0, 1], [30, 100]))
+    else:
+        clipped_brightness = self._offroad_brightness
 
     brightness = round(self._brightness_filter.update(clipped_brightness))
     if not self._awake:
-      brightness = 0
+        brightness = 0
 
     if brightness != self._last_brightness:
-      if self._brightness_thread is None or not self._brightness_thread.is_alive():
-        self._brightness_thread = threading.Thread(target=HARDWARE.set_screen_brightness, args=(brightness,))
-        self._brightness_thread.start()
-        self._last_brightness = brightness
+        if self._brightness_thread is None or not self._brightness_thread.is_alive():
+            self._brightness_thread = threading.Thread(target=HARDWARE.set_screen_brightness, args=(brightness,))
+            self._brightness_thread.start()
+            self._last_brightness = brightness
 
   def _update_wakefulness(self):
     # Handle interactive timeout
