00e05373c37db4ef2b53e124ff01297268589fba
Moodkiller <appleipodsrock@gmail.com>
2026-09-13 12:04:17 +1200

    Fix formatting in set_offroad_brightness method

--- selfdrive/ui/ui_state.py
+++ selfdrive/ui/ui_state.py
@@ -344,7 +344,7 @@ class Device:
     self._update_brightness()
     self._update_wakefulness()
 
-  def set_offroad_brightness(self, brightness: int | None):
+ def set_offroad_brightness(self, brightness: int | None):
     if brightness is None:
       brightness = BACKLIGHT_OFFROAD
     self._offroad_brightness = min(max(brightness, 0), 100)
